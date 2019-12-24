0258f95ea6052ecd5d27dc9a8b6d044fe7152309
hiranthar <sahanhirantha@gmail.com>
2019-12-24 19:06:43 +0530

    conf

--- src/Project/Home.java
+++ src/Project/Home.java
@@ -13,7 +13,6 @@ public class Home extends javax.swing.JFrame {
         initComponents();
     }
 
-
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
